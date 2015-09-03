312de6da7954e6227b3503054301333d0844bad3
Kamil Dworakowski <kamil@dworakowski.name>
2015-09-03 22:23:12 +0200

    add pvar

--- MyPrelude.hs
+++ MyPrelude.hs
@@ -5,21 +5,10 @@ module MyPrelude (
   Data.Traversable.foldMapDefault,
   fromMaybe, fromJust, maybeToList, mapMaybe, filterMap, isNothing, isJust, listToMaybe, catMaybes,
   E.throwIO, E.throw,
-  printf,
-  lookup,
-  lookupIn,
-  tryRead,
-  Failable(..), fail,
-  whenM, unlessM, ifM,
   List.nub, List.sort, List.partition, List.sortBy, List.groupBy, List.inits,
-  null,
   Data.Tuple.swap,
-  split,
-  removeOrFail,
-  readCommaDecimal,
   Data.Ord.comparing,
   Data.Ord.Down(..),
-  zlotowki
   ) where
 
 import Prelude as Exports hiding (mapM, mapM_, lookup, fail, null, all, any, product, sum, and, or,
@@ -48,85 +37,8 @@ import qualified Data.Char
 import qualified Data.Tuple
 import Data.Function as Exports
 import qualified Data.Ord
+import PVar as Exports
+import Utils as Exports
 
-class    Monad m => Failable m   where failErr :: Err.T -> m a
-instance Failable Maybe          where failErr _   = Nothing
-instance Failable (Either Err.T) where failErr err = Left err
-instance MonadIO m => Failable m where failErr err = liftIO (E.throwIO err)
-                                       
-fail :: Failable m => String -> m a
-fail = failErr . Err.Msg
-
-class OverloadedLookup t k v | t -> k, t -> v where overloadedLookup :: k -> t -> Maybe v
-instance (Eq k) => OverloadedLookup [(k,v)] k v where overloadedLookup = List.lookup
-instance (Ord k) => OverloadedLookup (M.Map k v) k v where overloadedLookup = M.lookup
-
-lookup :: (OverloadedLookup t k v, Show k, Show t, Failable m) => k -> t -> m v
-lookup k xs = lookupIn (shorten 256 (show xs)) k xs
-
--- this is a lookup variant for where there is no show instance for the collection
-lookupIn :: (OverloadedLookup t k v, Show k, Failable m) => String -> k -> t -> m v
-lookupIn name k = maybe (fail $ printf "Can't lookup %s in %s" (show k) name) return . overloadedLookup k
-                  
--- todo this might actually be faster if one does a linear search to see if the element is absent ahead of time
-removeOrFail :: (Show e, Failable m, Eq e) => e -> [e] -> m [e]
-removeOrFail el [] = fail ("Can't remove: no " ++ show el ++ " found")
-removeOrFail el (e:es) = if e == el then return es else removeOrFail el es >>= return . (e:)
-
-shorten :: Int -> String -> String
-shorten maxLen msg =
-  case List.splitAt maxLen msg of
-    (x, "") -> x
-    (x, _rest) -> take (maxLen - 5) x ++ "(...)"
-
-tryRead :: (Read a, Show a, Failable m) => String -> m a
-tryRead str =
-  case reads str of
-    [] -> fail ("Can't read: " ++ shorten 256 str)
-    [(a, rest)]
-      | all Data.Char.isSpace rest -> return a
-    parses -> fail ("Ambiguous parse: '" ++ str ++ "' " ++ shorten 256 (show parses))
-
-whenM :: MonadIO m => m Bool -> m () -> m ()
-whenM cond exec = cond >>= flip when exec
-  
-unlessM :: Monad m => m Bool -> m () -> m ()
-unlessM cond exec = cond >>= flip unless exec
-                    
-ifM :: Monad m => m Bool -> m a -> m a -> m a
-ifM cond thn els = do
-  cond <- cond
-  if cond then thn else els
-                  
-class Nullable c where
-  null :: c a -> Bool
-  
-instance Nullable [] where
-  null [] = True
-  null _ = False
-  
-instance Nullable Maybe where
-  null Nothing = True
-  null _ = False
-
-split :: (Char -> Bool) -> String -> [String]
-split _ [] = []
-split p str =
-  case break p str of
-   (x,"") -> [x]
-   (x, _:rest) -> x:split p rest
 
 filterMap = mapMaybe
-
-readCommaDecimal :: (Read a, RealFloat a) => String -> a
-readCommaDecimal =
-  read
-  . map commaToDot
-  . filter (/= ' ')
-  . filter (/= ' ') -- epromak's non-breaking space in thousands
- where
-   commaToDot ',' = '.'
-   commaToDot x = x
-
-zlotowki :: Double -> String
-zlotowki v = printf "%.2f zł" v -- use non-breaking space
---
+++ PVar.hs
@@ -0,0 +1,44 @@
+module PVar where
+
+import Prelude
+import Data.IORef
+import Control.Exception
+import Result
+import qualified Err
+import Text.Nicify (nicify)
+import Utils (tryRead)
+  
+data PVar a =
+  FSPVar (Maybe (UpgradeFun a)) FilePath
+  | IORefPVar (IORef a)
+    
+type UpgradeFun a = String -> Result a
+    
+newPVar :: FilePath -> PVar a
+newPVar = FSPVar Nothing
+            
+newPVarUpg :: UpgradeFun a -> FilePath -> PVar a
+newPVarUpg upg fp = FSPVar (Just upg) fp
+
+fakePVar :: a -> IO (PVar a)
+fakePVar x = IORefPVar <$> newIORef x
+            
+readPVar :: (Read a, Show a) => PVar a -> IO a
+readPVar (FSPVar mUpg fp) = do
+  content <- readFile fp
+  case tryRead content of
+   Right x -> return x
+   Left directReadErr ->
+     case mUpg of
+       Nothing -> throwIO (Err.tag ("while reading " ++ fp) directReadErr)
+       Just upg ->
+        case upg content of
+          Right x -> return x
+          Left err2 ->
+            throwIO (Err.tag ("can't read nor upgrade " ++ fp) (Err.List [directReadErr, err2]))
+readPVar (IORefPVar ioref) = readIORef ioref
+
+writePVar :: Show a => PVar a -> a -> IO ()
+writePVar (FSPVar _ fp) x =
+  writeFile fp (nicify . show $ x)
+writePVar (IORefPVar ioref) x = writeIORef ioref x
