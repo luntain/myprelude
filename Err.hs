e8f51cb2f7cc580a894d8dcd354ebf637ac2b259
Kamil Dworakowski <kamil@dworakowski.name>
2018-11-08 20:11:35 +0100

    adapt to changed in base 4.11 (ghc 8.4)

--- Err.hs
+++ Err.hs
@@ -3,6 +3,7 @@ module Err (T(..), pretty, throwIO, tag) where
 import Data.List
 import Data.Typeable
 import qualified Control.Exception as Exc
+import Data.Semigroup as Semi
 
 data T =
     Msg String
@@ -13,12 +14,15 @@ data T =
 instance Exc.Exception T where
   displayException = pretty 0
 
+instance Semi.Semigroup T where
+  List xs <> List ys = List (xs ++ ys)
+  List xs <> x       = List (xs ++ [x])
+  x       <> List xs = List (x:xs)
+  x       <> y       = List [x,y]
+
 instance Monoid T where
   mempty = List []
-  mappend (List xs) (List ys) = List (xs ++ ys)
-  mappend (List xs) x = List (xs ++ [x])
-  mappend x (List xs) = List (x:xs)
-  mappend x y = List [x,y]
+  mappend = (<>)
 
 pretty :: Int -> T -> String
 pretty indent (Msg str) = replicate indent ' ' ++ str
