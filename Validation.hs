055ec1ef1ee9baf7425539df50ca8cfed9b849f2
Kamil Dworakowski <kamil@dworakowski.name>
2018-11-09 16:31:34 +0100

    some breaking cleanup changes to Validation

--- Validation.hs
+++ Validation.hs
@@ -2,61 +2,57 @@ module Validation where
 
 import Utils
 import qualified Err
-import Control.Lens
+import Result
 
 
 -- <> is infixr 6 :|
 
 infix 4 >!
-(>!) :: (Ord a, Show a) => a -> a -> Err.T
-a >! b = if a > b then mempty else Err.Msg (show a ++ " is not greater than " ++ show b)
+(>!) :: (Ord a, Show a) => a -> a -> Result ()
+a >! b = if a > b then Right () else errorResult (show a ++ " is not greater than " ++ show b)
 
 infix 4 >=!
-(>=!) :: (Ord a, Show a) => a -> a -> Err.T
-a >=! b = if a >= b then mempty else Err.Msg (show a ++ " is not >= than " ++ show b)
+(>=!) :: (Ord a, Show a) => a -> a -> Result ()
+a >=! b = if a >= b then mempty else errorResult (show a ++ " is not >= than " ++ show b)
 
 infix 4 <!
-(<!) :: (Ord a, Show a) => a -> a -> Err.T
-a <! b = if a < b then mempty else Err.Msg (show a ++ " is not smaller than " ++ show b)
+(<!) :: (Ord a, Show a) => a -> a -> Result ()
+a <! b = if a < b then mempty else errorResult (show a ++ " is not smaller than " ++ show b)
 
 infix 4 <=!
-(<=!) :: (Ord a, Show a) => a -> a -> Err.T
-a <=! b = if a <= b then mempty else Err.Msg (show a ++ " is not <= than " ++ show b)
+(<=!) :: (Ord a, Show a) => a -> a -> Result ()
+a <=! b = if a <= b then mempty else errorResult (show a ++ " is not <= than " ++ show b)
 
 infix 4 =!
-(=!) :: (Eq a, Show a) => a -> a -> Err.T
-a =! b = if a == b then mempty else Err.Msg (show a ++ " is not equal to " ++ show b)
+(=!) :: (Eq a, Show a) => a -> a -> Result ()
+a =! b = if a == b then mempty else errorResult (show a ++ " is not equal to " ++ show b)
 
 infix 4 /=!
-(/=!) :: (Eq a, Show a) => a -> a -> Err.T
-a /=! b = if a/=b then mempty else Err.Msg (show a ++ " is equal to " ++ show b)
+(/=!) :: (Eq a, Show a) => a -> a -> Result ()
+a /=! b = if a/=b then mempty else errorResult (show a ++ " is equal to " ++ show b)
 
-isNothing :: Show a => Maybe a -> Err.T
-isNothing Nothing = mempty
-isNothing x = Err.Msg ("Expected Nothing, but got " ++ show x)
+ensureIsNothing :: Show a => Maybe a -> Result ()
+ensureIsNothing Nothing = mempty
+ensureIsNothing x = errorResult ("Expected Nothing, but got " ++ show x)
 
-isJust :: Maybe a -> Err.T
-isJust Nothing = Err.Msg ("Expected Just, but got Nothing")
-isJust (Just _) = mempty
+ensureIsJust :: Maybe a -> Result ()
+ensureIsJust Nothing = errorResult ("Expected Just, but got Nothing")
+ensureIsJust (Just _) = mempty
 
-all :: Show a => (a -> Err.T) -> [a] -> Err.T
+all :: Show a => (a -> Result ()) -> [a] -> Result ()
 all p = mconcat . map p'
   where p' x = if p x /= mempty
-                  then Err.tag (show x) (p x)
+                  then tag (show x) (p x)
                   else mempty
 
-ensure :: String -> Bool -> Err.T
-ensure label condition = if condition then mempty else Err.Msg label
+ensure :: String -> Bool -> Result ()
+ensure label condition = if condition then mempty else errorResult label
 
-validate :: Failable m => Err.T -> m ()
-validate e =
-  if e == mempty then return () else failErr e
-
-approxEqual :: (RealFrac a, Show a) => Double -> a -> a -> Err.T
+approxEqual :: (RealFrac a, Show a) => Double -> a -> a -> Result ()
 approxEqual ratio x y =
   if abs (realToFrac x - realToFrac y) < abs (ratio * avg)
     then mempty
-    else Err.Msg ("The numbers are too far apart: " ++ show x ++ " " ++ show y)
+    else errorResult ("The numbers are too far apart: " ++ show x ++ " " ++ show y)
   where
     avg :: Double
     avg = realToFrac (x + y) / 2
